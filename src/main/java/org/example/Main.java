40766895ec625e7c785d48689461f3a84264edf8
josemodolo <josenetozxc@yahoo.com>
2025-08-06 17:56:21 -0300

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.dio.desafio.dominio.*;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Mentoria mentoria = new Mentoria();
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Curso Java Dio");
+        curso1.setCargaHoraria(8);
+
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descricao mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJose = new Dev();
+        devJose.setNome("Jose");
+        devJose.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos" + devJose.getConteudoInscritos());
+        devJose.progredir();
+        System.out.println("Conteúdos Incritos" + devJose.getConteudoInscritos());
+        System.out.println("Conteúdos Concluidos" + devJose.getConteudosConcluidos());
+
+
+        Dev devCamila = new Dev();
+        devJose.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println(("Conteúdos Incritos" + devCamila.getConteudoInscritos()));
+        devCamila.progredir();
+        System.out.println("Conteúdos Concluidos" + devCamila.getConteudosConcluidos());
+
+
+
     }
 }
